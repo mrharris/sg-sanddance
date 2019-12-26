01081cd52d94344836ace3c17e1fbcea9e490b6e
Luke Harris <lukeha@gmail.com>
2019-12-26 16:03:45 +0000

    Basic react-sanddance

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "@msrvida/sanddance-explorer/dist/css/sanddance-explorer.css";
+import * as deck from "@deck.gl/core";
+import * as layers from "@deck.gl/layers";
+import * as luma from "luma.gl";
+import * as fabric from "office-ui-fabric-react";
+import * as vega from "vega";
+import { Explorer, use } from "@msrvida/sanddance-explorer";
+import "./styles.css";
+
+fabric.initializeIcons();
+
+use(fabric, vega, deck, layers, luma);
+
+const data = [
+  { a: 1, b: "c1", c: 10 },
+  { a: 1, b: "c2", c: 20 },
+  { a: 2, b: "c3", c: 30 },
+  { a: 3, b: "c4", c: 40 }
+];
+
+const explorerProps = {
+  logoClickUrl: "https://microsoft.github.io/SandDance/",
+  mounted: explorer => {
+    explorer.load(data);
+  }
+};
+
+ReactDOM.render(
+  <Explorer {...explorerProps} />,
+  document.getElementById("app")
+);
